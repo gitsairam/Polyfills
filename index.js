9795bcc917f0757d52444173bebe493f3d08959e
gitsairam <sairamvenkata194@gmail.com>
2022-03-28 20:47:25 +0530

    1 file modified

--- index.js
+++ index.js
@@ -33,3 +33,41 @@ console.log(
     return el > 2;
   })
 );
+
+Array.prototype.myReduce = function (cb, initialValue) {
+  let accumulator = initialValue;
+  for (let i = 0; i < this.length; i++) {
+    accumulator = accumulator ? cb(accumulator, this[i], i, this) : this[i];
+  }
+  return accumulator;
+};
+
+var arr = [
+  { shape: 'square', color: 'red', used: 1, instances: 1 },
+  { shape: 'square', color: 'red', used: 2, instances: 1 },
+  { shape: 'circle', color: 'blue', used: 0, instances: 0 },
+  { shape: 'square', color: 'blue', used: 4, instances: 4 },
+  { shape: 'circle', color: 'red', used: 1, instances: 1 },
+  { shape: 'circle', color: 'red', used: 1, instances: 0 },
+  { shape: 'square', color: 'blue', used: 4, instances: 5 },
+  { shape: 'square', color: 'red', used: 2, instances: 1 },
+];
+
+var obj = {};
+
+var helper = {};
+var result = arr.reduce(function (r, o) {
+  var key = o.shape + '-' + o.color;
+
+  if (!helper[key]) {
+    helper[key] = Object.assign({}, o); // create a copy of o
+    r.push(helper[key]);
+  } else {
+    helper[key].used += o.used;
+    helper[key].instances += o.instances;
+  }
+
+  return r;
+}, []);
+
+console.log(result);
